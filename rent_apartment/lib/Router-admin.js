1323f0f1d0a92260407fb189de955a8e759b7323
sonvisal <sonvisal@konpig.(none)>
2015-12-23 13:12:04 +0700

    change menu

--- rent_apartment/lib/Router-admin.js
+++ rent_apartment/lib/Router-admin.js
@@ -1,10 +1,24 @@
 
-Router.route('/admin/dashboard', {
-    name: 'dashboard'
-});
+// Router.route('/admin/dashboard', {
+//     name: 'dashboard'
+// });
 Router.route('/admin/manageuser', {
     name: 'manageuser'
 });
 Router.route('/admin/managebrand', {
     name: 'managebrand'
+});
+Router.map(function () {
+	this.route('/admin', {
+		name: 'dashboard',
+		onBeforeAction: function (pause) {
+			if (!Meteor.user()) {
+				// render the login template but keep the url in the browser the same
+				Session.set('dashboard',1);
+				this.render('login');
+			} else {
+				this.next();
+			}
+		}
+	})
 });
